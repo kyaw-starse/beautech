0b41c9d43aacf8474cfce108e148f9b8b1ab1899
kyaw-starse <101231054+kyaw-starse@users.noreply.github.com>
2025-11-29 11:10:12 +0000

    メトロ調整

--- src/components/about/AccessSectionComponent.tsx
+++ src/components/about/AccessSectionComponent.tsx
@@ -12,7 +12,7 @@ const AccessSectionComponent = () => {
         <span>所在地：</span>
         <span>東京都中央区築地6丁目1-9 <br className="md:hidden" />門跡木村ビル2F</span>
       </p>
-      <div className="w-full h-[300px] md:h-[570px] px-0 md:px-12 lg:px-16">
+      <div className="w-full h-[285px] md:h-[570px] px-0 md:px-12 lg:px-16">
         <iframe
           src="https://www.google.com/maps?q=東京都中央区築地6丁目1-9 門跡木村ビル2F&hl=ja&output=embed"
           width="100%"
@@ -25,7 +25,7 @@ const AccessSectionComponent = () => {
       </div>
       <div className="text-sm md:text-base leading-loose flex flex-row items-start mb-5 md:mb-8 mt-5 md:mt-0">
         <span>メトロ：</span>
-        <p className='inline'>築地駅 徒歩5分<br className="md:hidden" /><span className="hidden md:inline">、</span>東銀座 徒歩10分</p>
+        <p className='inline'>築地駅「日比谷線」1番出口より 徒歩約5分<br className="md:hidden" /><span className="hidden md:inline">、</span>東銀座駅「浅草線、日比谷線」5番出口より　徒歩10分</p>
       </div>
       </div>
     </div>
